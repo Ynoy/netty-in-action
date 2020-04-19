91ccdcb57b7553153c5a4fc841c7d1bf49f72beb
CheneyIn <xy410257@163.com>
2020-04-19 20:16:50 +0800

    debug源码

--- chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
+++ chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
@@ -16,17 +16,23 @@ public class BlockingIoExample {
 
     /**
      * Listing 1.1 Blocking I/O example
+     *
      * */
     public void serve(int portNumber) throws IOException {
+        // 创建一个新的ServerSocket，用于监听指定端口上的请求连接
         ServerSocket serverSocket = new ServerSocket(portNumber);
+        // accept()将被阻塞，直到一个连接建立，将返回一个新的Socket用于客户端与服务器之间的通信。
+        // 同时ServerSocket继续监听传入的连接。
         Socket clientSocket = serverSocket.accept();
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out =
                 new PrintWriter(clientSocket.getOutputStream(), true);
         String request, response;
+        // 开始循环处理
         while ((request = in.readLine()) != null) {
             if ("Done".equals(request)) {
+                // 直至客户端发送Done停止循环
                 break;
             }
             response = processRequest(request);
